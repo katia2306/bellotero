16de7f4145511fd6f64caf3c5b4dc2095fc73435
Katia Araceli Vega Soto <kavegas@concredito.com.mx>
2020-06-30 17:59:00 -0600

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Router, Switch, Route } from "react-router";
 import history from "./history";
 import NavBar from "./NavBar";
 import Testimonal from "./Testimonal";
-import Configurator from "./Calculator";
+import Calculator from "./Calculator";
 
 class App extends PureComponent {
   render() {
@@ -14,7 +14,7 @@ class App extends PureComponent {
           <Switch>
             <Route exact path="/" component={Testimonal} />
             <Route exact path="/page-1" component={Testimonal} />
-            <Route exact path="/page-2" component={Configurator} />
+            <Route exact path="/page-2" component={Calculator} />
           </Switch>
         </Router>
       </>
